6ddab85727ff769946a7da8c9d2de59e931c7f4b
Ciprian <botnari_ciprian@mail.ru>
2022-11-01 08:43:07 +0200

    Lab 5 initial commit

---
+++ lab_5/chess/src/main/java/com/chess/logic/board/Square.java
@@ -0,0 +1,71 @@
+package com.chess.logic.board;
+
+import com.chess.logic.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Square {
+    protected final int squareCoordinate;
+
+    private static final Map<Integer, EmptySquare> EMPTY_SQUARES = createAllEmptySquares();
+
+    private static Map<Integer,EmptySquare> createAllEmptySquares() {
+        final Map<Integer, EmptySquare> emptySquareMap = new HashMap<>();
+
+        for(int i = 0; i < 64; i++) {
+            emptySquareMap.put(i, new EmptySquare(i));
+        }
+
+        return ImmutableMap.copyOf(emptySquareMap);
+    }
+
+    public static Square createSquare(final int squareCoordinate, Piece piece) {
+        return piece != null ? new OccupiedSquare(squareCoordinate, piece) : EMPTY_SQUARES.get(squareCoordinate);
+    }
+
+    private Square(int squareCoordinate) {
+        this.squareCoordinate = squareCoordinate;
+    }
+
+    public abstract boolean isSquareOccupied();
+    public abstract Piece getPiece();
+
+    // Empty Square
+    public static final class EmptySquare extends Square {
+        EmptySquare(final int coordinate) {
+            super(coordinate);
+        }
+
+        @java.lang.Override
+        public boolean isSquareOccupied() {
+            return false;
+        }
+
+        @java.lang.Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    // Occupied Square
+    public static final class OccupiedSquare extends Square {
+        private final Piece pieceSquare;
+
+        OccupiedSquare(int squareCoordinate, Piece pieceSquare) {
+            super(squareCoordinate);
+            this.pieceSquare = pieceSquare;
+        }
+
+        @java.lang.Override
+        public boolean isSquareOccupied() {
+            return true;
+        }
+
+        @java.lang.Override
+        public Piece getPiece() {
+            return this.pieceSquare;
+        }
+    }
+}
---
+++ lab_5/chess/src/main/java/com/chess/logic/pieces/Piece.java
@@ -0,0 +1,5 @@
+package com.chess.logic.pieces;
+
+public class Piece {
+
+}
